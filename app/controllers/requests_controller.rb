8eeb06b003d5fb50ed27309125ddaef4a94a6245
Pedro Diaz <diaz2209@gmail.com>
2016-03-28 01:14:28 -0430

    ultimos ajustes con funcionalidad de solicitudes

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,6 @@
 class RequestsController < ApplicationController
   before_action :validate_authentication
-  before_action :validate_admin_access, only: [:edit,:destroy]  
+  before_action :validate_member_access, only: [:edit]  
   before_action :set_request, only: [:show, :edit, :update, :destroy]
   before_action :set_profiles, only: [:index, :new,:edit, :update]
   before_action :set_comunities, only: [:index, :new,:edit, :update]
