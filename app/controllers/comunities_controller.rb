96d1a7737e481b90c6d60c395586b550d63e0c65
Pedro Diaz <diaz2209@gmail.com>
2016-03-29 01:03:39 -0430

    Agregando opcion comunidad para miembro

--- app/controllers/comunities_controller.rb
+++ app/controllers/comunities_controller.rb
@@ -1,5 +1,7 @@
 class ComunitiesController < ApplicationController
   before_action :validate_authentication
+  before_action :validate_admin_access, only: [:index]
+  before_action :validate_member_specifyc_access, except: [:index]
   before_action :set_comunity, only: [:show, :edit, :update, :destroy]
   before_action :set_location_values, only: [:index, :new,:edit, :update]
   # GET /comunities
@@ -71,6 +73,17 @@ class ComunitiesController < ApplicationController
       @states = State.all
       @towns = Town.all
       @parishes = Parish.all
+    end
+
+    def validate_member_specifyc_access
+      @comunity = Comunity.friendly.find(params[:id])
+      if  (current_user.type.nombre!="Administrador" or session[:type_user]!="Administrador")
+        if (current_user.type.nombre=="Miembro" and session[:type_user]=="Miembro")
+          if(@comunity.id!=current_user.profile.member.comunity_id)
+            redirect_to(root_path,alert: "Lo sentimos, no tiene permisos para acceder esta seccion")
+          end
+        end
+      end
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def comunity_params
